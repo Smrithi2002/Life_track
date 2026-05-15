17988c4ecccd65c490ce279967659e690e107578
smrithi <smrithi.s2002@gmail.com>
2026-05-15 14:23:17 +0530

    api documentation creation

--- medconnect/urls.py
+++ medconnect/urls.py
@@ -29,6 +29,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 
 urlpatterns = [
     # Django Admin Panel
@@ -52,6 +54,11 @@ urlpatterns = [
 
     # Billing (invoices, payments)
     path('api/v1/', include('billing.urls')),
+
+     # ... existing URLs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 # Serve media files in development
